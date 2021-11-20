422d22b8a1a36147d9cfe38a706ca309b7d7da66
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-11-19 23:20:10 -0500

    feat(day-1): added day 1
    Added source code for day 1, which contains an introduction to comments
    and System.out.println().

---
+++ day-1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // this is a comment
+
+    // the Java compiler will ignore these, so you can use comments to write
+    // notes about what the code does or for anything else you might find useful
+
+    // this statement prints out the string "Hello World"
+    // you can use System.out.println() at any time to print something to the
+    // console what you want to print goes inside the brackets
+    System.out.println("Hello world!");
+  }
+}
