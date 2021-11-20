0fcd731c4f752e73447ebdc5b24df66e8e749f4d
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-11-19 23:24:08 -0500

    docs(README): created README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Beginner Lessons
+Source code for any and all beginner lessons run during Hack Club, sorted by day. 
--- day-1/src/com/company/Main.java
+++ day-1/src/com/company/Main.java
@@ -1,12 +1,19 @@
-package com.company;
+package com.company; // ignore this for now
 
+// by the way, this is is a comment (preceded by two forward slashes). The Java
+// compiler will ignore these, so you can use comments to write notes about what
+// the code does or for anything else you might find useful
+
+// this is the class declaration. You don't need to worry about it for now, but
+// just know the name of the class (right after the word class), must be the
+// same as the name of your file. Example: the name of this class is Main, and
+// the name of the file is Main.java
 public class Main {
 
+  // this is the main function. Anything that goes in between the two curly
+  // brackets here is run, so this is where you'll want to put all your code
+  // there (at least for now)
   public static void main(String[] args) {
-    // this is a comment
-
-    // the Java compiler will ignore these, so you can use comments to write
-    // notes about what the code does or for anything else you might find useful
 
     // this statement prints out the string "Hello World"
     // you can use System.out.println() at any time to print something to the
