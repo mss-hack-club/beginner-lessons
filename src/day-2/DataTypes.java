974dd5682bc56cf5288696e07653ba42236c275b
Sonia Sharma <71413895+soniasharma12@users.noreply.github.com>
2021-12-05 01:51:14 -0500

    Update DataTypes.java

--- src/day-2/DataTypes.java
+++ src/day-2/DataTypes.java
@@ -1,17 +1,17 @@
 public class DataTypes {
 
   public static void main(String[] args) {
-    int myNum = 5;           // declare integer
-    float floatNum = 5.99f;  // declare float
-    char myCharacter = '%';  // declare character
-    boolean myBool = true;   // declare boolean
-    String myText = "Hello"; // declare string
+    int myNum = 5; // declare integer
+    float floatNum = 5.99f;// declare float
+    char myCharacter = '%';// declare character
+    boolean myBool = true; // declare boolean
+    String myText = "Hello";// declare string
 
-    System.out.println(myNum);
-    System.out.println(floatNum);
-    System.out.println(myBool);
-    System.out.println(myCharacter);
-    System.out.println(myText);
-    System.out.println(myNum + " " + myText);
+    System.out.println(myNum);// output integer variable
+    System.out.println(floatNum);// output float variable
+    System.out.println(myBool);// output boolean variable
+    System.out.println(myCharacter);// output char variable
+    System.out.println(myText);// output string variable
+    System.out.println(myNum + " " + myText);// outpitn integer and string variable
   }
 }
